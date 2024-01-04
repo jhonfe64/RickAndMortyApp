233a23b271e241333debbea4426ecc16351fb7cf
john <jhonfe649@gmail.com>
2024-01-04 02:41:10 -0500

    fix routres

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import SearchBar from "./components/searchBar/SearchBar";
-import Filtered from "./components/filtered/filtered";
+import Filtered from "./components/filtered/Filtered";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Details from "./components/detail/Details";
